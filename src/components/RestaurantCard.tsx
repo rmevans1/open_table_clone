b92e3d34254e426711e02d736298221e8881421a
Robert Evans <robertevans@robertevansmb.com>
2023-05-09 10:01:38 -0400

    Price

--- src/components/RestaurantCard.tsx
+++ src/components/RestaurantCard.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import {RestaurantCardType} from "@/app/page";
+import Price from "@/components/Price";
 
 interface Props {
     restaurant: RestaurantCardType;
@@ -24,7 +25,7 @@ export default function RestaurantCard({restaurant}: Props) {
                     </div>
                     <div className="flex text-reg font-light capitalize">
                         <p className=" mr-3">{restaurant.cuisine.name}</p>
-                        <p className="mr-3">$$$$</p>
+                        <Price price={restaurant.price} />
                         <p>{restaurant.location.name}</p>
                     </div>
                     <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
