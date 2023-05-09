d9d815a162162fc35cd736aa370042aaf621d37e
Robert Evans <robertevans@robertevansmb.com>
2023-05-09 12:30:29 -0400

    Remove logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,8 +41,6 @@ const inter = Inter({subsets: ['latin']})
 
 export default async function Home() {
     const restaurants = await fetchRestaurants();
-
-    console.log({restaurants})
   return (
       <>
         <main>
