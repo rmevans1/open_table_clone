49753664bcd839d30d492c9f6eb37f6baab19714
Robert Evans <robertevans@robertevansmb.com>
2023-05-09 11:34:28 -0400

    Remove logging

--- src/app/restaurant/[slug]/page.tsx
+++ src/app/restaurant/[slug]/page.tsx
@@ -45,7 +45,7 @@ export const metadata = {
 
 export default async function RestaurantDetails({params}: {params: {slug: string}}) {
     const restaurant = await fetchRestaurantBySlug(params.slug);
-    console.log(restaurant);
+
     return (
         <>
             <div className="bg-white w-[70%] rounded p-3 shadow">
