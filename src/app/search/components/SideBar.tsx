f3948e2a4c5e169346ccd962497e251697ca354f
Robert Evans <robertevans@robertevansmb.com>
2023-05-09 15:04:26 -0400

    Challenge 4

--- src/app/search/components/SideBar.tsx
+++ src/app/search/components/SideBar.tsx
@@ -1,20 +1,44 @@
-export default function SideBar() {
+import {PrismaClient} from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const fetchCities = async () => {
+    const cities = prisma.location.findMany({
+        select: {
+            id: true,
+            name: true,
+        }
+    });
+
+    return cities;
+}
+
+const fetchCuisines = async () => {
+    const cuisines = prisma.cuisine.findMany({
+        select: {
+            id: true,
+            name: true,
+        }
+    });
+
+    return cuisines;
+}
+export default async function SideBar() {
+    const cities = await fetchCities();
+    const cuisines = await fetchCuisines();
     return (
         <div className="w-1/5">
             <div className="border-b pb-4">
                 <h1 className="mb-2">Region</h1>
-                <p className="font-light text-reg">Toronto</p>
-                <p className="font-light text-reg">Ottawa</p>
-                <p className="font-light text-reg">Montreal</p>
-                <p className="font-light text-reg">Hamilton</p>
-                <p className="font-light text-reg">Kingston</p>
-                <p className="font-light text-reg">Niagara</p>
+                {cities.map((city => (
+                    <p className="font-light text-reg capitalize" key={city.id}>{city.name}</p>
+                )))}
             </div>
             <div className="border-b pb-4 mt-3">
                 <h1 className="mb-2">Cuisine</h1>
-                <p className="font-light text-reg">Mexican</p>
-                <p className="font-light text-reg">Italian</p>
-                <p className="font-light text-reg">Chinese</p>
+                {cuisines.map((cuisine => (
+                    <p className="font-light text-reg capitalize" key={cuisine.id}>{cuisine.name}</p>
+                )))}
             </div>
             <div className="mt-3 pb-4">
                 <h1 className="mb-2">Price</h1>
